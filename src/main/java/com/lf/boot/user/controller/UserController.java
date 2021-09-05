0f9858e859a6b62cab4baa411de09802cd3b5e0b
liufan <liufan@qq.com>
2021-09-05 19:27:08 +0800

    简单使用redis。。。。。。。。。。。。

--- src/main/java/com/lf/boot/user/controller/UserController.java
+++ src/main/java/com/lf/boot/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.lf.boot.user.controller;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.lf.boot.base.controller.BaseController;
+import com.lf.boot.redis.RedisUtils;
 import com.lf.boot.user.entity.User;
 import com.lf.boot.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,27 @@ public class UserController extends BaseController<User> {
 
     private final IUserService userService;
 
+    private final RedisUtils redisUtils;
+
     @Autowired
-    public UserController(IUserService userService) {
+    public UserController(IUserService userService, RedisUtils redisUtils) {
         this.userService = userService;
+        this.redisUtils = redisUtils;
     }
 
     @Override
     public IService<User> getService() {
         return userService;
+    }
+
+    @RequestMapping("/redis/set")
+    public void setRedis(String key, String val) {
+        redisUtils.set(key, val);
+    }
+
+    @RequestMapping("/redis/get")
+    public String getRedis(String key) {
+        return redisUtils.get(key);
     }
 
 }
